9cdb4b3fa4a2689a26b3cb9bfcf211050f710237
Lucas Nicolau <lucas@octha.com>
2022-03-16 23:30:27 -0300

    ✨ adding redirect location parameter

--- config/sso.php
+++ config/sso.php
@@ -2,10 +2,11 @@
 
 // sso server uri
 $server = 'http://localhost/sso/';
+$redirect = 'http://localhost/';
 
 return [
-    'route' => array_map(function ($route) use ($server) {
-        return $server . $route . '?client=' . ($_SERVER['HTTP_HOST'] ?? '');
+    'route' => array_map(function ($route) use ($server, $redirect) {
+        return $server . $route . '?client=' . ($_SERVER['HTTP_HOST'] ?? '') . '&r=' . $redirect;
     }, [
         // identifies if the user is logged in and handles the return
         'identifier'    => 'identifier',
